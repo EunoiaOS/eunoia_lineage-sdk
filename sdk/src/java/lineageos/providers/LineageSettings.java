b11f90d17541c6cbbe5fcbc6c14f81332a6bbfae
EunoiaOS <235215905+eunoia-committer@users.noreply.github.com>
2026-02-01 12:35:29 +0700

    sdk: Add EunoiaBattery style [1/3]
    Change-Id: I68aa7e1af16cbd9d2fe77387a7f0de9e866332b4

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -993,14 +993,15 @@ public final class LineageSettings {
          * Display style of the status bar battery information
          * 0: Display the battery an icon in portrait mode
          * 1: Display the battery as a circle
-         * 2: Display the battery as plain text
+         * 2: Display the Eunonia's battery icon
+         * 3: Display the battery as plain text
          * default: 0
          */
         public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
 
         /** @hide */
         public static final Validator STATUS_BAR_BATTERY_STYLE_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 2);
+                new InclusiveIntegerRangeValidator(0, 3);
 
         /**
          * Status bar battery %
