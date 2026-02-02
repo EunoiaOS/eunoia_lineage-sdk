73a534908c34c0bb20c6a270ea624f8fb51bc4a2
EunoiaOS <235215905+eunoia-committer@users.noreply.github.com>
2026-02-02 12:35:27 +0700

    Remove Lineage CircleBatteryDrawable
    Change-Id: I9f26d378ef4d1ec174997cf31998be36de815277

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -991,17 +991,16 @@ public final class LineageSettings {
 
         /**
          * Display style of the status bar battery information
-         * 0: Display the battery an icon in portrait mode
-         * 1: Display the battery as a circle
-         * 2: Display the Eunonia's battery icon
-         * 3: Display the battery as plain text
+         * 0: Display the Eunonia's battery circle icon
+         * 1: Display the battery an icon in portrait mode
+         * 2: Display the battery as plain text
          * default: 0
          */
         public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
 
         /** @hide */
         public static final Validator STATUS_BAR_BATTERY_STYLE_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 3);
+                new InclusiveIntegerRangeValidator(0, 2);
 
         /**
          * Status bar battery %
